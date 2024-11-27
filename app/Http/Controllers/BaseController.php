d8f918bdf85b3b0dadbd93bc484b8ebcab2eb063
aryaddinata <aryaddinataabbas017@gmail.com>
2024-11-28 04:13:39 +0900

    fix : polyline

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -19,92 +19,88 @@ class BaseController extends Controller
 
     public function home()
     {
-        return view ('/pages/home',[
-            "datapendudukL"=>Datapenduduk::where('jenis_kelamin','LAKI-LAKI')->count(),
-            "datapendudukP"=>Datapenduduk::where('jenis_kelamin','PEREMPUAN')->count(),
-            "data" => SambutanLurah::where('id',1)->get(),
-            "datapenduduk"=>Datapenduduk::count(),
-            "info"=>Posts::latest()->get(),
-            "posts"=>Posts::latest()->paginate(6),
-            "banner"=>Banner::latest()->get(),
-            'title'=>'Website Resmi Kelurahan Seli'
+        return view('/pages/home', [
+            "datapendudukL" => Datapenduduk::where('jenis_kelamin', 'LAKI-LAKI')->count(),
+            "datapendudukP" => Datapenduduk::where('jenis_kelamin', 'PEREMPUAN')->count(),
+            "data" => SambutanLurah::where('id', 1)->get(),
+            "datapenduduk" => Datapenduduk::count(),
+            "info" => Posts::latest()->get(),
+            "posts" => Posts::latest()->paginate(6),
+            "banner" => Banner::latest()->get(),
+            'title' => 'Website Resmi Kelurahan Tomalou'
         ]);
     }
 
     public function dashboard()
     {
-        return view('dashboardd.index',[
-            "alluser"=>User::all()->count(),
-            "penduduk"=>Datapenduduk::all()->count(),
-            "posts"=>Posts::count(),
+        return view('dashboardd.index', [
+            "alluser" => User::all()->count(),
+            "penduduk" => Datapenduduk::all()->count(),
+            "posts" => Posts::count(),
         ]);
     }
 
 
     public function visimisi()
     {
-        return view('pages.profil.visimisi',[
-            'data'=>Visimisi::get(),
-            'title'=> 'Visi dan Misi - Website Resmi Kelurahan Seli'
+        return view('pages.profil.visimisi', [
+            'data' => Visimisi::get(),
+            'title' => 'Visi dan Misi - Website Resmi Kelurahan Tomalou'
         ]);
     }
 
     public function sejarah()
     {
-        return view('pages.profil.sejarah',[
-            'data'=>Sejarah::get(),
-            'title'=>'Sejarah Kelurahan Seli - Website Resmi Kelurahan Seli'
+        return view('pages.profil.sejarah', [
+            'data' => Sejarah::get(),
+            'title' => 'Sejarah Kelurahan Seli - Website Resmi Kelurahan Tomalou'
         ]);
     }
     public function gambaranumum()
     {
-        return view('pages.profil.gambaranumum',[
-            'data'=>GambaranUmum::get(),
-            'title'=>'Gambaran Umum kelurahan Seli - Website Resmi Kelurahan Seli'
+        return view('pages.profil.gambaranumum', [
+            'data' => GambaranUmum::get(),
+            'title' => 'Gambaran Umum kelurahan Seli - Website Resmi Kelurahan Tomalou'
         ]);
     }
 
     public function perangkat()
     {
-        return view('pages.profil.perangkat',[
-            "perangkat"=>PerangkatKelurahan::where('id',1)->get(),
-            'title'=>'Perangkat Kelurahan Seli - Website Resmi Kelurahan Seli'
+        return view('pages.profil.perangkat', [
+            "perangkat" => PerangkatKelurahan::where('id', 1)->get(),
+            'title' => 'Perangkat Kelurahan Seli - Website Resmi Kelurahan Tomalou'
         ]);
     }
-       
+
     public function lpm()
     {
-        return view('pages.kelembagaan.lpm',[
-            "lpm"=>Lpm::get(),
-            'title'=>'Lembaga Pemberdayaan Masyarakat - Website Resmi Kelurahan Seli'
+        return view('pages.kelembagaan.lpm', [
+            "lpm" => Lpm::get(),
+            'title' => 'Lembaga Pemberdayaan Masyarakat - Website Resmi Kelurahan Tomalou'
         ]);
     }
 
     public function pkk()
     {
-        return view('pages.kelembagaan.pkk',[
-            "pkk"=>Pkk::get(),
-            'title'=>'Pemberdayaan Kesejahteraan Keluarga - Website Resmi Kelurahan Seli'
+        return view('pages.kelembagaan.pkk', [
+            "pkk" => Pkk::get(),
+            'title' => 'Pemberdayaan Kesejahteraan Keluarga - Website Resmi Kelurahan Tomalou'
         ]);
     }
 
     public function informasi()
     {
-        return view('pages.informasi.informasi',[
-            "posts"=>Posts::latest()->paginate(10),
-            "posts_new"=>Posts::latest()->paginate(10),
-            'title'=> 'Informasi Seputar Kelurahan Seli - Website Resmi Kelurahan Seli'
+        return view('pages.informasi.informasi', [
+            "posts" => Posts::latest()->paginate(10),
+            "posts_new" => Posts::latest()->paginate(10),
+            'title' => 'Informasi Seputar Kelurahan Seli - Website Resmi Kelurahan Tomalou'
         ]);
     }
     public function detail_informasi(Posts $posts)
     {
-        return view('pages.informasi.detailInformasi',[
-            'title'=> $posts->judul.'- Website Resmi Kelurahan Seli',
-            'posts'=>$posts
+        return view('pages.informasi.detailInformasi', [
+            'title' => $posts->judul . '- Website Resmi Kelurahan Tomalou',
+            'posts' => $posts
         ]);
     }
-
-
-
-
 }
--- public/assets/js/my.js
+++ public/assets/js/my.js
@@ -1,22 +1,22 @@
 
-var peta1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var peta1 = L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+} );
 
 var peta2 = L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/satellite-v9'
-    });
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/satellite-v9'
+} );
 
 
-var map = L.map('map', {
-    center: [0.6231582842470047, 127.40284978058301],
+var map = L.map( 'map', {
+    center: [ 0.6231582842470047, 127.40284978058301 ],
     zoom: 17,
-    layers: [peta1],
-});
+    layers: [ peta1 ],
+} );
 
 var baseLayers = {
     'Default': peta1,
@@ -24,195 +24,173 @@ var baseLayers = {
 
 };
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+} ).addTo( map );
 
-var lc1 = L.icon({
-    iconUrl: './icon/masjid.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6222759385134449, 127.40352027793243], {
+var lc1 = L.icon( {
+    iconUrl: './icon/mosque-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6222759385134449, 127.40352027793243 ], {
     icon: lc1
-}).addTo(map).bindPopup("<b> Masjid Nurul Bahar</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> Masjid Nurul Bahar</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc2 = L.icon({
-    iconUrl: './icon/masjid.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6233119317340542, 127.40756905696031], {
+var lc2 = L.icon( {
+    iconUrl: './icon/mosque-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6233119317340542, 127.40756905696031 ], {
     icon: lc2
-}).addTo(map).bindPopup("<b> Masjid Darussalam</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> Masjid Darussalam</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc3 = L.icon({
-    iconUrl: './icon/masjid.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6246896117786915, 127.39681555812109], {
+var lc3 = L.icon( {
+    iconUrl: './icon/mosque-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6246896117786915, 127.39681555812109 ], {
     icon: lc3
-}).addTo(map).bindPopup("<b> Masjid Besar Tomalou</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> Masjid Besar Tomalou</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-// var lc4 = L.icon({
-//     iconUrl: './icon/masjid.png',
-//     iconSize: [30, 40],
-// })
-// L.marker([0.6389027381818648, 127.43419785557376], {
-//     icon: lc4
-// }).addTo(map).bindPopup("<b> Mushola Al-Ikhlas</b><br>" +
-//     "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+var lc3 = L.icon( {
+    iconUrl: './icon/landmark-flag-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6222069530334474, 127.40370896445833 ], {
+    icon: lc3
+} ).addTo( map ).bindPopup( "<b> FKNT </b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-// var lc5 = L.icon({
-//     iconUrl: './icon/posyandu.png',
-//     iconSize: [30, 40],
-// })
-// L.marker([0.637253952433538, 127.43282322345681], {
-//     icon: lc5
-// }).addTo(map).bindPopup("<b> Posyandu</b><br>" +
-//     "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
-
-var lc6 = L.icon({
-    iconUrl: './icon/kantor.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6224109322592779, 127.40425400880235], {
+var lc6 = L.icon( {
+    iconUrl: './icon/house-laptop-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6224109322592779, 127.40425400880235 ], {
     icon: lc6
-}).addTo(map).bindPopup("<b> Kantor Lurah Tomalou</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> Kantor Lurah Tomalou</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc7 = L.icon({
-    iconUrl: './icon/polkesdes.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6234952470688616, 127.39874370543703], {
+var lc7 = L.icon( {
+    iconUrl: './icon/house-medical-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6234952470688616, 127.39874370543703 ], {
     icon: lc7
-}).addTo(map).bindPopup("<b> Puskesmas Tomalou </b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> Puskesmas Tomalou </b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc8 = L.icon({
-    iconUrl: './icon/sekolah.png',
-    iconSize: [25, ],
-})
-L.marker([0.6247436984923774, 127.40582475484308], {
+var lc8 = L.icon( {
+    iconUrl: './icon/school-solid.svg',
+    iconSize: [ 25, ],
+} )
+L.marker( [ 0.6247436984923774, 127.40582475484308 ], {
     icon: lc8
-}).addTo(map).bindPopup("<b> MTs Tomalou</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> MTs Tomalou</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc9 = L.icon({
-    iconUrl: './icon/lapangan.png',
-    iconSize: [30, 40],
-})
-L.marker([0.623745530242941, 127.39901496951565], {
+var lc9 = L.icon( {
+    iconUrl: './icon/futbol-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.623745530242941, 127.39901496951565 ], {
     icon: lc9
-}).addTo(map).bindPopup("<b> Stadion Grecele </b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b> Stadion Grecele </b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc10 = L.icon({
-    iconUrl: './icon/sekolah.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6241074853517081, 127.3997121434504], {
+var lc10 = L.icon( {
+    iconUrl: './icon/school-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6241074853517081, 127.3997121434504 ], {
     icon: lc10
-}).addTo(map).bindPopup("<b>TK Pembina 9 Tidore Kepulauan</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b>TK Pembina 9 Tidore Kepulauan</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
-var lc11 = L.icon({
-    iconUrl: './icon/sekolah.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6250971616572779, 127.39783459713983], {
+var lc11 = L.icon( {
+    iconUrl: './icon/school-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6250971616572779, 127.39783459713983 ], {
     icon: lc11
-}).addTo(map).bindPopup("<b>SMPN 3 Tidore Kepulauan</b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
+} ).addTo( map ).bindPopup( "<b>SMPN 3 Tidore Kepulauan</b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
 
-var lc14 = L.icon({
-    iconUrl: './icon/sekolah.png',
-    iconSize: [30, 40],
-})
-L.marker([0.6259357474861693, 127.39697752647083], {
+var lc14 = L.icon( {
+    iconUrl: './icon/school-solid.svg',
+    iconSize: [ 30, 40 ],
+} )
+L.marker( [ 0.6259357474861693, 127.39697752647083 ], {
     icon: lc14
-}).addTo(map).bindPopup("<b> SMKN 2 Tidore Kepulauan </b><br>" +
-    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara").addTo(map);
-
-
-
+} ).addTo( map ).bindPopup( "<b> SMKN 2 Tidore Kepulauan </b><br>" +
+    "Tomalou, Tidore Selatan, Kota Tidore Kepulauan, Maluku Utara" ).addTo( map );
 
 //polyline
 var batas_tomalou = [
-    [0.6225812942083822 , 127.4094108898464 ],
-    [0.6231037795519817 , 127.4094765810955 ],
-    [0.6365173045242508 , 127.4070810131274 ],
-    [0.6371229699784573 , 127.3971183446679 ],
-    [0.6248436897773102 , 127.3945957186075 ],
-    [0.6241239071791291 , 127.3944666165787 ],
-    [0.6237854034600686 , 127.3951528094753 ],
-    [0.6234269398591995 , 127.3977435139597 ],
-    [0.6221594312450419 , 127.400090386906 ],
-    [0.6218118871455125 , 127.4024900985665 ],
-    [0.6219059334524856 , 127.4040668143016 ],
-    [0.6221639609929737 , 127.4044245481791 ],
-    [0.6219275473658825 , 127.4058306047337 ],
-    [0.6229003073554876 , 127.4068112182403 ],
-    [0.6227398032431336 , 127.4086112242956 ],
-    [0.6225812942083822 , 127.4094108898464 ],
 
+    [ 0.660059, 127.402738 ],
+    [ 0.6262162306932322, 127.3974625864109 ],
+    [ 0.6236753324483093, 127.3967316402622 ],
+    [ 0.623497, 127.397076 ],
+    [ 0.6235020475824422, 127.3973188707473 ],
+    [ 0.6234804739824865, 127.3976582800187 ],
+    [ 0.6233838848180293, 127.3978559899735 ],
+    [ 0.6231409055004888, 127.3980856441154 ],
+    [ 0.6229584640390546, 127.3984354286694 ],
+    [ 0.6229732985147254, 127.3984960611503 ],
+    [ 0.6225061427029029, 127.3993472092668 ],
+    [ 0.622528055877995, 127.3995281455841 ],
+    [ 0.6224946062223921, 127.3996531917188 ],
+    [ 0.6223789192824062, 127.3997925024844 ],
+    [ 0.6222485885880147, 127.4003142307216 ],
+    [ 0.6222300396219279, 127.400747938973 ],
+    [ 0.6221844767663679, 127.4010626937352 ],
+    [ 0.6221085111056915, 127.4014513351278 ],
+    [ 0.6219241899726788, 127.401707666409 ],
+    [ 0.6219145460835627, 127.4030320597159 ],
+    [ 0.6219149734372869, 127.4040182009288 ],
+    [ 0.6222106381097996, 127.4040663114855 ],
+    [ 0.6221973200470466, 127.4042849778844 ],
+    [ 0.6221523665911541, 127.4044291067611 ],
+    [ 0.6221752964071995, 127.4045490098845 ],
+    [ 0.6221685765436564, 127.4046067464172 ],
+    [ 0.62209271233124, 127.4046212464573 ],
+    [ 0.6219595422804637, 127.4056514684775 ],
+    [ 0.6219999649000429, 127.4057977661352 ],
+    [ 0.6224429626638563, 127.4061691050507 ],
+    [ 0.6224664186557594, 127.4062974522508 ],
+    [ 0.6225401730218076, 127.4064229619077 ],
+    [ 0.6228629137100631, 127.4066669443373 ],
+    [ 0.6228860236260013, 127.4072267591548 ],
+    [ 0.6223340742993869, 127.4072183666666 ],
+    [ 0.6223418417329351, 127.4073643771655 ],
+    [ 0.6224997595715797, 127.407359398714 ],
+    [ 0.6225187066357731, 127.4074681863063 ],
+    [ 0.6226092102696941, 127.4074733245635 ],
+    [ 0.6226218247529456, 127.4073318282359 ],
+    [ 0.6228443771926349, 127.4073561280529 ],
+    [ 0.622768058664476, 127.4082371907838 ],
+    [ 0.6227130244966922, 127.4087004799442 ],
+    [ 0.623881000270871, 127.4088904118094 ],
+    [ 0.6243967379461095, 127.4089292685106 ],
+    [ 0.660059, 127.402738 ],
 ];
-L.polyline(batas_tomalou, {
-    color: 'red'
-}).addTo(map);
+L.polygon( batas_tomalou, {
+    color: 'red',
+    fillColor: 'red',
+    fillOpacity: 0.1,
+    weight: 2
+} ).addTo( map );
 
-//batas rt
-// var batas_RT01 = [
-//     [0.639897192489887, 127.42904142123223],
-//     [0.6391220448195741, 127.42957401336926],
-//     [0.6387481420576728, 127.42960673634016],
-//     [0.6382402195284854, 127.43018702010339],
-//     [0.6372504510282576, 127.4320168931152],
-//     [0.6370774477625348, 127.4326865530009],
-//     [0.6368467786453189, 127.43299353687613],
-// ];
-// L.polyline(batas_RT01, {
-//     color: 'blue'
-// }).addTo(map);
+var layerControl = L.control.layers( baseLayers ).addTo( map );
 
-// var batas_RT02 = [
-//     [0.6405596753542633, 127.43040522755837],
-//     [0.6397064720072245, 127.43098449997468],
-//     [0.6392416983005842, 127.43152388852185],
-//     [0.639054515194737, 127.43132625117407],
-//     [0.6384739570152349, 127.4321307508226],
-//     [0.6378569488852489, 127.43315673013406],
-//     [0.6378988857128304, 127.43322114085984],
-//     [0.6375530623839976, 127.43361001215352],
-// ];
-// L.polyline(batas_RT02, {
-//     color: 'blue'
-// }).addTo(map);
+var map = L.map( 'map' ).setView( [ 0.6391968705884246, 127.4337552206041 ], 16 );
 
-// var batas_RT03 = [
-//     [0.6413501174433845, 127.43200969700392],
-//     [0.6400058805812904, 127.4331570002829],
-//     [0.639487900792424, 127.4334422197372],
-//     [0.6393728084084235, 127.43345575845544],
-//     [0.6390899142752772, 127.43366501194278],
-//     [0.6391242208973449, 127.43371644363144],
-//     [0.6386276035312201, 127.434102402754],
-//     [0.6386387803728619, 127.43420062972551],
-//     [0.6383260435314276, 127.43450284581861],
-// ];
-// L.polyline(batas_RT03, {
-//     color: 'blue'
-// }).addTo(map);
-
-var layerControl = L.control.layers(baseLayers).addTo(map);
-
-var map = L.map('map').setView([0.6391968705884246, 127.4337552206041], 16);
-
-var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var tiles = L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 25,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+} ).addTo( map );
